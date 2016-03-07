291bac582fbd6183364a64108d618d74a362b246
Fony Lew <kamolphan@hotmail.com>
2016-03-07 14:44:41 +0000

    init project

---
+++ centrality.R
@@ -0,0 +1,13 @@
+library(RNeo4j)
+library(igraph)
+library(jsonlite)
+neo4j = startGraph("http://104.197.136.113:7474/db/data/")
+query =  "
+MATCH (p1:User)-[:POSTED]->(:Topic)<-[:REPLIED]-(p2:User)
+WHERE p1.id < p2.id
+RETURN p1.id, p2.id, COUNT(*) AS weight
+order by weight desc
+"
+data = cypher(neo4j, query)
+g = graph.data.frame(data, directed = F)
+sort(betweenness(g), decreasing = T)[1:10]
