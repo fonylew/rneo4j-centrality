e0bae543ebea27d17aea3bb30cc9428340a403e7
Fony Lewprasert <kamolphan.fony@gmail.com>
2016-03-13 11:57:41 +0700

    edit degree query and update all

--- centrality.R
+++ centrality.R
@@ -1,7 +1,21 @@
 library(RNeo4j)
 library(igraph)
 library(jsonlite)
-neo4j = startGraph("http://104.197.136.113:7474/db/data/")
+neo4j = startGraph("http://104.197.210.78:7474/db/data/")
+
+query0 =  "
+MATCH (p1:User)-[:POSTED]->(:Topic)<-[:REPLIED]-(p2:User)
+WHERE p1.id < p2.id
+RETURN p1.id, p2.id, COUNT(*) AS weight
+order by weight desc
+"
+data0 = cypher(neo4j, query0)
+g0 = graph.data.frame(data0, directed = F)
+
+deg = degree(g0)
+summary(deg)
+maxdegree(deg)
+
 query =  "
 MATCH (p1:User)-[:POSTED]->(:Topic)<-[:REPLIED]-(p2:User)
 WHERE p1.id < p2.id
@@ -10,29 +24,17 @@ order by weight desc
 "
 data = cypher(neo4j, query)
 g = graph.data.frame(data, directed = F)
+
 bet = betweenness(g)
 summary(bet)
 maxbet = max(bet)
-for(i in 1:length(bet))
-{
-    unode = getLabeledNodes(neo4j, "User", id = as.integer(names(bet[i]))) 
-    updateProp(unode[[1]],betweenness_centrality = as.numeric(bet[[i]]),betweenness = as.numeric(bet[[i]])/maxbet)
-}
 
 close = closeness(g)
 summary(close)
 maxclose = max(close)
-for(i in 1:length(close))
-{
-    unode = getLabeledNodes(neo4j, "User", id = as.integer(names(close[i]))) 
-    updateProp(unode[[1]],closeness_centrality = as.numeric(close[[i]]),closeness = as.numeric(close[[i]])/maxclose)
-}
 
-deg = degree(g)
-summary(deg)
-maxdegree(deg)
 for(i in 1:length(deg))
 {
     unode = getLabeledNodes(neo4j, "User", id = as.integer(names(deg[i])))
-    updateProp(unode[[1]],degree = as.numeric(deg[[i]]))
+    updateProp(unode[[1]],degree = as.numeric(deg[[i]]),betweenness_centrality = as.numeric(bet[[i]]),betweenness = as.numeric(bet[[i]])/maxbet, closeness_centrality = as.numeric(close[[i]]),closeness = as.numeric(close[[i]])/maxclose)
 }
