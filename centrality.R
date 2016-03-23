3bffaaa0350234d3d953f0e4b26b1bad7076c57a
Fony Lewprasert <kamolphan.fony@gmail.com>
2016-03-23 20:23:10 +0700

    update centrality.R

--- centrality.R
+++ centrality.R
@@ -7,8 +7,8 @@ setwd('~/rneo4j-centrality')
 getwd()
 neo4j = startGraph("http://104.197.210.78:7474/db/data/")
 
-Sys.time()
 
+system.time({
 queryu =  "
 MATCH (p1:User)-[:POSTED]->(:Topic)<-[r:REPLIED]-(p2:User)
 WHERE p1.id < p2.id
@@ -21,18 +21,21 @@ gu = graph.data.frame(datau, directed = F)
 deg = degree(gu)
 summary(deg)
 maxdegree=max(deg)
+})
+
+system.time({
 for(i in 1:length(deg))
 {
     unode = getLabeledNodes(neo4j, "User", id = as.integer(names(deg[i])))
     updateProp(unode[[1]],degree = as.numeric(deg[[i]]))
 }
+})
 
 print('completed User degree')
 save(deg, file="degree_user.Rdata")
 sort_deg=sort(deg,decreasing=TRUE)  
 write.csv(sort_deg,"degree_user_sort.csv")  
 Sys.time()
-
 # BETWEENNESS & CLOSENESS
 
 system.time({
@@ -52,11 +55,13 @@ summary(bet)
 maxbet = max(bet)
 })
 
+system.time({
 for(i in 1:length(bet))
 {
     unode = getLabeledNodes(neo4j, "User", id = as.integer(names(bet[i]))) 
-    updateProp(unode[[1]],betweenness_centrality = as.numeric(bet[[i]]),betweenness = as.numeric(bet[[i]])/maxbet)
+    updateProp(unode[[1]],betweenness_centrality = as.numeric(bet[[i]]))
 }
+})
 
 print('completed betweenness')
 save(bet, file="betweenness_centrality.Rdata")
@@ -90,12 +95,10 @@ Sys.time()
 # print('completed writing node User')
 # Sys.time()
 
-
-
 system.time({
 queryt =  "
 MATCH (n:User)-[r:REPLIED]->(p:Topic)
-WITH count(r) as weight,p.id as pid
+WITH count(*) as weight,p.id as pid
 RETURN pid,weight
 order by weight desc
 "
@@ -116,34 +119,16 @@ Sys.time()
 
 print('- Finished -')
 
+
+print('Graph Density')
+system.time({
+density=graph.density(g, loops=FALSE)
+})
+density
+
+# It's take really long time
 print('calculating Graph Diameter')
 system.time({
-di=diameter(g)
+diameter=diameter(g)
 })
-
-###########################
-save(ebet, file="estimate_betweenness.Rdata")
-max_ebet = max(ebet)
-min_ebet = min(ebet)
-norm_ebet = (ebet-min_ebet)/(max_ebet-min_ebet)
-
-system.time({
-for(i in 1:length(ebet))
-{
-    unode = getLabeledNodes(neo4j, "User", id = as.integer(names(ebet[i]))) 
-    updateProp(unode[[1]],estimate_betweenness = as.numeric(ebet[[i]]),norm_ebet = as.numeric(norm_ebet[[i]]))
-}
-})
-
-system.time({
-close = estimate_closeness(g,cutoff=-1, weights = NULL,normalize=TRUE)
-summary(close)
-})
-
-system.time({
-for(i in 1:length(close))
-{
-    unode = getLabeledNodes(neo4j, "User", id = as.integer(names(close[i]))) 
-    updateProp(unode[[1]],normalize_closeness = as.numeric(close[[i]]))
-}
-})
+diameter
---
+++ korn.R
@@ -0,0 +1,74 @@
+library(RNeo4j)
+library(igraph)
+library(igraph)
+library(jsonlite)
+neo4j = startGraph("http://104.197.210.78:7474/db/data/")
+#################################
+
+# Food
+query_food =  
+"
+MATCH (n:User)-[p:POSTED]->(t:Topic)-[c:CLASSED]->(g:Room {name: 'food'})
+WITH n.id as nid, g.name as room, count(*) as sum , SUM(t.degree)as deg_room, n.degree as ndegree , n.betweenness_centrality as nbetween, n.closeness_centrality as ncloseness
+RETURN nid,room,deg_room,sum ,ndegree,nbetween,ncloseness
+order by deg_room desc
+"
+data_food = cypher(neo4j, query_food)
+View(data_food)
+
+# blueplanet
+query_blueplanet =  
+"
+MATCH (n:User)-[p:POSTED]->(t:Topic)-[c:CLASSED]->(g:Room {name: 'blueplanet'})
+WITH n.id as nid, g.name as room, count(*) as sum , SUM(t.degree)as deg_room, n.degree as ndegree , n.betweenness_centrality as nbetween, n.closeness_centrality as ncloseness
+RETURN nid,room,deg_room,sum ,ndegree,nbetween,ncloseness
+order by deg_room desc
+"
+data_blueplanet = cypher(neo4j, query_blueplanet)
+View(data_blueplanet)
+
+# sinthorn
+query_sinthorn =  
+  "
+MATCH (n:User)-[p:POSTED]->(t:Topic)-[c:CLASSED]->(g:Room {name: 'sinthorn'})
+WITH n.id as nid, g.name as room, count(*) as sum , SUM(t.degree)as deg_room, n.degree as ndegree , n.betweenness_centrality as nbetween, n.closeness_centrality as ncloseness
+RETURN nid,room,deg_room,sum ,ndegree,nbetween,ncloseness
+order by deg_room desc
+"
+data_sinthorn = cypher(neo4j, query_sinthorn)
+View(data_sinthorn)
+
+# chalermthai
+query_chalermthai =  
+  "
+MATCH (n:User)-[p:POSTED]->(t:Topic)-[c:CLASSED]->(g:Room {name: 'chalermthai'})
+WITH n.id as nid, g.name as room, count(*) as sum ,n.degree as ndegree , n.betweenness_centrality as nbetween, n.closeness_centrality as ncloseness
+RETURN nid,room,sum ,ndegree,nbetween,ncloseness
+order by sum desc
+"
+data_chalermthai = cypher(neo4j, query_chalermthai)
+View(data_chalermthai)
+
+# isolate
+query_isolate =  
+  "
+MATCH (n:User)-[p:POSTED]->(t:Topic)-[c:CLASSED]->(g:Room {name: 'isolate'})
+WITH n.id as nid, g.name as room, count(*) as sum , SUM(t.degree)as deg_room, n.degree as ndegree , n.betweenness_centrality as nbetween, n.closeness_centrality as ncloseness
+RETURN nid,room,deg_room,sum ,ndegree,nbetween,ncloseness
+order by deg_room desc
+"
+data_isolate = cypher(neo4j, query_isolate)
+View(data_isolate)
+
+
+##TAG
+user="327698"          #change id here
+query_tag = paste(
+"
+MATCH (n:User{id:",user,"})-[p:POSTED]->(t:Topic)-[:TAGGED]->(g:Tag)
+WITH g.name as tag , count(t)as count_tag
+RETURN tag,count_tag
+order by count_tag desc
+")
+data_tag = cypher(neo4j, query_tag)
+View(data_tag)
